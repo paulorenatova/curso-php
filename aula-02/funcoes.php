61130aa5a4b7c6255ae7c594a3b1ea18f0adfe70
paulorenatova <paulorenatova@gmail.com>
2018-05-02 20:07:01 -0300

    Criacao do arquivo funcoes
    funcoes

---
+++ aula-02/funcoes.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Paulo - Curso
+ * Date: 02/05/2018
+ * Time: 20:06
+ */
--- aula-02/loops.php
+++ aula-02/loops.php
@@ -1,7 +1,2 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Paulo - Curso
- * Date: 02/05/2018
- * Time: 19:16
- */
+
