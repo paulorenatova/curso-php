ca5ec6e6478f0e6e2333d745fabd2ea58b3b4307
Tiago Gouvêa <tiago@tiagogouvea.com.br>
2018-05-07 21:01:16 -0300

    Bootstrap adicionado

--- aula-03-tiago/menu.php
+++ aula-03-tiago/menu.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
 <hr>
 <a href="alunos.php">Alunos</a> |
 <a href="cursos.php">Cursos</a> |
